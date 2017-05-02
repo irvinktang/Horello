c08b2b9664a5ad63d157a380351f2275dccc5e7b
Irvin <irvink.tang@gmail.com>
2017-05-02 12:29:23 -0700

    edit

--- js/js/config.js
+++
@@ -1,6 +0,0 @@
-
-
-var apiKey = "ac960adda2eff64acbbd98e795c025ca";
-var apiToken = "4f056833805f0d9f9942665e386fb731be47e335e7b9a32a5abb87e1e2ea8125";
-var apiUrl = "https://api.trello.com/1";
-var boardId = "588577bb8423080722cabe8c"
